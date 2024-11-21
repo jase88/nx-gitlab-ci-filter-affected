83cb6563d280fa629f7bcaae9c0f5b2e6fb0c068
jase88 <804836+jase88@users.noreply.github.com>
2024-11-21 09:18:29 +0100

    init

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["./src/nx-gitlab-ci-filter-affected.ts"],
+  format: ["esm"],
+  platform: "node",
+  minify: true,
+  splitting: false,
+  clean: true,
+  publicDir: false,
+});
