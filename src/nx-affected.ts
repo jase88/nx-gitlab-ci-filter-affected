51e59eef8b609ca2de177e9a74af4c6b5c950984
jase88 <804836+jase88@users.noreply.github.com>
2025-09-10 21:01:58 +0200

    refactor: improve error handling and simplify project retrieval logic

--- src/nx-affected.ts
+++ src/nx-affected.ts
@@ -3,23 +3,24 @@ import { promisify } from 'node:util';
 
 const execPromise = promisify(exec);
 
-async function runNxShowProjectsAffected(target: string, projects?: Set<string>): Promise<Set<string>> {
+async function runNxShowProjectsAffected(target: string, projects?: Set<string>) {
 	const projectsFilter = projects ? ` --projects=${Array.from(projects).join(',')}` : '';
 	const nxShowProjectsAffected = `npx nx show projects --affected --json --with-target=${target}${projectsFilter}`;
 
 	try {
-		const { stdout, stderr } = await execPromise(nxShowProjectsAffected);
-		if (stderr) {
-			throw new Error(stderr);
-		}
-
-		const projectsAffected = JSON.parse(stdout.toString());
-		return new Set(projectsAffected);
+		return execPromise(nxShowProjectsAffected);
 	} catch (error) {
 		throw new Error(`Error occurred calling "${nxShowProjectsAffected}": ${error.message}`);
 	}
 }
 
-export function getAffectedProjectsByTarget(target: string, projects?: Set<string>) {
-	return runNxShowProjectsAffected(target, projects);
+export async function getAffectedProjectsByTarget(target: string, projects?: Set<string>) {
+	const { stdout, stderr } = await runNxShowProjectsAffected(target, projects);
+
+	if (stderr) {
+		throw new Error(stderr);
+	}
+
+	const projectsAffected = JSON.parse(stdout.toString());
+	return new Set(projectsAffected);
 }
